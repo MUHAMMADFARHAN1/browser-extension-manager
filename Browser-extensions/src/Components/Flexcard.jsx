07da4ceae3003297d781f4ddd4de856e676fe6af
Muhammad Farhan <farhan_75230@hotmail.com>
2025-04-26 12:49:55 +0200

    props added to the card.

--- Browser-extensions/src/Components/Flexcard.jsx
+++ Browser-extensions/src/Components/Flexcard.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Switch } from "@material-tailwind/react";
 
-function Flexcard() {
+function Flexcard(props) {
   return (
     <div className="bg-neutral-500 px-10 py-10 w-80 rounded-2xl flex flex-col">
       <div className="flex flex-row">
         <img src="./../../assets/images/logo-dom-snapshot.svg" alt="" />
         <div className="pl-5">
-          <h1 className="text-white">DevLens</h1>
-          <p className="text-white">
-            Quickly inpsect page layouts and visualize element boundaries
-          </p>
+          <h1 className="text-white">{props.heading}</h1>
+          <p className="text-white">{props.text}</p>
         </div>
       </div>
       <div className="flex flex-row w-50 justify-between pt-4">
--- Browser-extensions/src/Components/Flexcard_Container.jsx
+++ Browser-extensions/src/Components/Flexcard_Container.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Flexcard from "./Flexcard";
+import array from "../../../data";
+// 1.Javascript arraz or 2.json server needs to be created here or 3. simplz copy everzthing
 
 function Flexcard_Container() {
+  // const listItems = array.map((item) => <Flexcard className=" col-span-1" heading="{}" text="{}" />);
+
   return (
     <div className="grid grid-cols-3 gap-10">
-      <Flexcard className=" col-span-1" />
+      <Flexcard className=" col-span-1" heading="dsgfd" text="dsfdsf" />
       <Flexcard className=" col-span-1" />
       <Flexcard className=" col-span-1" />
 
@@ -20,3 +24,5 @@ function Flexcard_Container() {
 }
 
 export default Flexcard_Container;
+
+// if page is empty check devtools and then the log is to be seen
